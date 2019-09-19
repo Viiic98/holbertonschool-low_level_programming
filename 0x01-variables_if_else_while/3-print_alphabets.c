9731207146cdd2a3a20dbcf0a4097c095be75353
viiic98 <xathovic@gmail.com>
2019-09-19 21:52:16 +0000

    In lowercase and upercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always(Success)
+ */
+int main(void)
+{
+	char a = 'a';
+	char i = 'z';
+
+	while (a <= i)
+	{
+		putchar(a);
+		a++;
+	}
+
+	a = 'A';
+	i = 'Z';
+
+	while (a <= i)
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
