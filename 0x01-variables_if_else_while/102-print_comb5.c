24a9db0d3cdf668ada3000c8804e9baefb2d55c8
viiic98 <xathovic@gmail.com>
2019-09-21 02:44:36 +0000

    Four numbers are displayed

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if ((i != j || i != k || i != l || j != k || j != l || k != l)
+					    && l > j && k >= i)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
+
+						if (i != 9 || j != 8 || k != 9 || l != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
