c0755da6e6af85acc613e1d7d333a28f4c8e84fb
Victor Arteaga <xathovic@gmail.com>
2019-09-24 18:11:54 +0000

    Final hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,32 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d, f = 0;
+	int a, b;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 00; a <= 23; a++)
 	{
-		for (b = 0; b <= 9 && f != 1; b++)
+		for (b = 00; b <= 59; b++)
 		{
-			for (c = 0; c <= 5 && f != 1; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a != 2 || b != 4 || c != 0 || d != 0)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-					else
-					{
-						f = 1;
-						break;
-					}
-				}
-			}
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
