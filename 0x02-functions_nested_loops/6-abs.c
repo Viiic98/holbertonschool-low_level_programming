926a2b2dd234c7c0f6280313a83aa7b78a612010
Victor Arteaga <xathovic@gmail.com>
2019-09-24 14:53:25 +0000

    Fixing the absolute valor

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,15 @@
 #include "holberton.h"
-
+/**
+ * _abs - The function for absolute valor
+ *
+ * @n: is the variable number
+ *
+ * Return: Always the absolute number for the input
+ */
 int _abs(int n)
 {
 	int a;
+
 	if (n < 0)
 	{
 		a = n * -1;
