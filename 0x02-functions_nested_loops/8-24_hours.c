efcc8263d1abb717086d6dcd9208ebdf673dd2b2
Victor Arteaga <xathovic@gmail.com>
2019-09-24 16:41:02 +0000

    Another fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,17 +14,14 @@ void jack_bauer(void)
 		{
 			for (c = 0; c <= 5; c++)
 			{
-				for (d = 0; d <= 9 && a <= 2 && b <= 3; d++)
+				for (d = 0; a <= 2 && b <= 3 && c <= 5 && d <= 9; d++)
 				{
-					if (a != 2 || b != 4 || c != 0 || d != 0)
-					{
 						_putchar(a + '0');
 						_putchar(b + '0');
 						_putchar(':');
 						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
-					}
 				}
 			}
 		}
