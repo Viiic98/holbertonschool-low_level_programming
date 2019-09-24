e4dae8be095adcd5274cbf973ba55304c6353c0f
Victor Arteaga <xathovic@gmail.com>
2019-09-24 17:13:19 +0000

    Fixing the hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,22 +6,30 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a, b, c, d, f = 0;
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b <= 9 && f != 1; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			for (c = 0; c <= 5 && f != 1; c++)
 			{
-				for (d = 0; a <= 2 && b <= 3 && c <= 5 && d <= 9; d++)
+				for (d = 0; d <= 9; d++)
 				{
+					if (a != 2 || b != 4 || c != 0 || d != 0)
+					{
 						_putchar(a + '0');
 						_putchar(b + '0');
 						_putchar(':');
 						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
+					}
+					else
+					{
+						f = 1;
+						break;
+					}
 				}
 			}
 		}
