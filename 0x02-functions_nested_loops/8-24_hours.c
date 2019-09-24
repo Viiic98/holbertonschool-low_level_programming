19ac823c9b75be485ceeee3bb4b1c7764839c36e
Victor Arteaga <xathovic@gmail.com>
2019-09-24 15:41:07 +0000

    Displaying the hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+ * jack_bauer - Function for display the seconds
+ *
+ * Return: Second by second
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d, finish;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a != 2 || b != 4 || c != 0 || d != 0)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
+					else
+					{
+						finish = 1;
+						break;
+					}
+				}
+				if (finish == 1)
+				{
+					break;
+				}
+			}
+			if (finish == 1)
+			{
+				break;
+			}
+		}
+	}
+
+}
