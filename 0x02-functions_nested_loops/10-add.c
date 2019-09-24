f0e74cf6b9174de4aca02ad213e20f2225f220db
Victor Arteaga <xathovic@gmail.com>
2019-09-24 16:55:10 +0000

    Add between two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * add - function for add two digits
+ *
+ * @a: first input variable
+ * @b: second input variable
+ *
+ * Return: The result of add two integers
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+
+	return (res);
+}
