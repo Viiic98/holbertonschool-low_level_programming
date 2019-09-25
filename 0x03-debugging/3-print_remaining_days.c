e49f7ec88dac5901d55d75058c89fae2e500b9e6
viiic98 <xathovic@gmail.com>
2019-09-25 04:26:18 +0000

    Fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,9 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (month >= 1 && month <= 12 && day >= 1 && day <= 365)
-	{
-		if ((year % 4) == 0 && (year % 100) == 0 && (year % 400) == 0)
+	if ((year % 4) == 0 || ((year % 100) == 0 && (year % 400) == 0)))
 		{
 			if (month >= 2 && day >= 60)
 			{
@@ -35,5 +33,4 @@ void print_remaining_days(int month, int day, int year)
 				printf("Remaining days: %d\n", 365 - day);
 			}
 		}
-	}
 }
