2ce6a1944600b049d23d0c808e0e3ab6cf819245
viiic98 <xathovic@gmail.com>
2019-09-25 03:45:05 +0000

    fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,11 +19,10 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-		
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else 
+	else
 	{
 		if (month == 2 && day == 60)
 		{
