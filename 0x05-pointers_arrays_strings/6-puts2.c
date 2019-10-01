9cc61c741314fdb1389ca93896ed177267d2d029
Victor Arteaga <xathovic@gmail.com>
2019-10-01 18:06:13 +0000

    Updating every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * puts2 - Function
+ *
+ * @str: Input variable
+ *
+ * Return: Every other character
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
