7bb72c2c4301d9dac94c8986d18089eb99213caa
Victor Arteaga <xathovic@gmail.com>
2019-10-03 16:25:38 +0000

    Adding the second file with the header file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
 /**
+ * _strncpy - Function that copy two strings
  *
+ * @dest: String that will store the copied string
+ * @src: String that will be copied
+ * @n: Number of characters that will be copied
  *
- *
- *
+ * Return: The string copied
  */
 char *_strncpy(char *dest, char *src, int n)
 {
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,8 +2,12 @@
 #define HOLBERTON_H
 
 /* Concatenate two strings */
-char *_strcat(char *dest, char *src);
+char *_strcat(char*, char*);
 
-/* */
-char *_strncat(char *dest, char *src, int n);
+/* Concatenating two words with specific number of characters */
+char *_strncat(char*, char*, int n);
+
+/* Function that copies a string */
+char *_strncpy(char*, char*, int);
+
 #endif /* HOLBERTON_H */
