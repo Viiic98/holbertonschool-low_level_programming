dce85965af02d32847f07d1ecd040a1d2a0848f5
Victor Arteaga <xathovic@gmail.com>
2019-10-07 23:47:10 +0000

    Another fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,10 +19,12 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[j])
 		{
 			j++;
-			if (haystack[i] == haystack[i + 1])
-			{
-				i = i + 1;
-			}
+			i++;
+			if (haystack[i] == haystack[i - 1])
+			{}
+			else
+				i--;
+
 			if (needle[j] == '\0')
 			{
 				return (haystack + (i - (j - 1)));
