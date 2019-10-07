c722f21fb21246a4c0a2af464ed62135f80cadd1
Victor Arteaga <xathovic@gmail.com>
2019-10-07 19:17:02 +0000

    Seting a pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * set_string - Seting a string
+ *
+ *
+ * @s: String that will be seted
+ * @to: String that will be copied
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -13,4 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 /* Printing after one specific character */
 char *_strchr(char *s, char c);
 
+/*  */
+unsigned int _strspn(char *s, char *accept);
+
+/* sets the value of a char */
+void set_string(char **s, char *to);
 #endif /* HOLBERTON.H */
