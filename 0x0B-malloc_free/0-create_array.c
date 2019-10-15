f8c6cb93df7d821e526e6b1b7259a4e9de460ed6
Victor Arteaga <xathovic@gmail.com>
2019-10-15 15:42:21 +0000

    Creating an array with malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - Create an array using malloc
+ * fill an array with a input value
+ *
+ * @size: size of the array
+ * @c: value that will be asign
+ *
+ * Return: pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	a = malloc(size * sizeof(c));
+
+	for (i = 0; i < size; i++)
+		a[i] = c;
+
+	return (a);
+}
