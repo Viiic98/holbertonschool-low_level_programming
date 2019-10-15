09b0299a7b70a84abbfdc78f6797580d9b6502a2
Victor Arteaga <xathovic@gmail.com>
2019-10-15 22:35:08 +0000

    Another fix without calloc u.u

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,21 +10,21 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **a, i;
+	int **a, i, j;
 
-	if (width <= 0 || height <= 0)
+	if (width == 0 || height == 0)
 		return (NULL);
 
-	a = calloc(width, sizeof(int *) * width);
-
+	a = malloc(width * sizeof(int *));
 	if (a == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		a[i] = calloc(height, height * sizeof(int));
-		if (a[i] == NULL)
-			return (NULL);
+		a[i] = malloc(height * sizeof(char));
+		for (j = 0; j < height; j++)
+			a[i][j] = 0;
 	}
+
 	return (a);
 }
