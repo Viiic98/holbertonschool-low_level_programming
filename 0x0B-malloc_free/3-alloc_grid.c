d1b1a6e69573a28772a58a747cb34b979867545f
Victor Arteaga <xathovic@gmail.com>
2019-10-15 18:01:46 -0500

    Fixing

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 {
 	int **a, i, j;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	a = malloc(height * sizeof(int *));
