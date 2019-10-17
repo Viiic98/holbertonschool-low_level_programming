0e5f4e486168edffa99082bb50621d7d3c2f76f4
Victor Arteaga <xathovic@gmail.com>
2019-10-17 16:01:11 +0000

    My calloc function_

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - Allocate memory and initialize in 0
+ *
+ * @nmemb: size of array
+ * @size: Data type
+ *
+ * Return: pointer to the array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int i;
+
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		a[i] = 0;
+
+	return (a);
+}
