077a429125c0421e88635981b503ca9a6884eb8e
Victor Arteaga <xathovic@gmail.com>
2019-10-17 18:55:28 +0000

    Remaked

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *a;
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb <= 0 || size == 0)
 		return (NULL);
 
 	a = malloc(nmemb * size);
@@ -21,6 +21,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
+	{
 		a[i] = 0;
+	}
+
 	return (a);
 }
