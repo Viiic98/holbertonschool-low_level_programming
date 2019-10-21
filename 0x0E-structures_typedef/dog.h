df7cbfad042b7452ea4de2b59d0b0591aee23df2
Victor Arteaga <xathovic@gmail.com>
2019-10-21 20:43:26 +0000

    Typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,13 @@
  * @age: value age
  * @owner: owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
