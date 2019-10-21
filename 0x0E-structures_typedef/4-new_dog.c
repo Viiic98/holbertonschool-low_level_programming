cd9dc93a27fd773052070b8cf800867ba476b167
Victor Arteaga <xathovic@gmail.com>
2019-10-21 22:14:56 +0000

    fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,20 +31,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	c_name = malloc(sizeof(char) * l_n + 1);
 	if (c_name == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	c_owner = malloc(sizeof(char) * l_o + 1);
 	if (c_owner == NULL)
+	{
+		free(new);
+		free(c_name);
 		return (NULL);
-
+	}
 	for (i = 0; i < l_n; i++)
 	{
 		c_name[i] = name[i];
 	}
+	c_name[i] = '\0';
 	for (i = 0; i < l_o; i++)
 	{
 		c_owner[i] = owner[i];
 	}
-
+	c_owner[i] = '\0';
 	(*new).name = c_name;
 	(*new).age = age;
 	(*new).owner = c_owner;
