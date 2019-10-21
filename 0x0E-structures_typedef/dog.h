c2196b9ddf670d1783264b23f2a9f3209d4aeef8
Victor Arteaga <xathovic@gmail.com>
2019-10-21 19:51:01 +0000

    Using types from structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - First structure
+ * @name: value name
+ * @age: value age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int _putchar(char);
+#endif
