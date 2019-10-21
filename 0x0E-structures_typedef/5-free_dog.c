adfed6bf9cd5bf358c36d882ff7284ed9f002f7f
Victor Arteaga <xathovic@gmail.com>
2019-10-21 22:25:06 +0000

    Free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Freedom
+ *
+ * @d: address
+ * Return: nothing, is void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != '\0')
+	{
+		free((*d).name);
+		free((*d).owner);
+	}
+}
