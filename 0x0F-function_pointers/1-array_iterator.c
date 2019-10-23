139030325f7dcd0fb100c1f8187a79c09cfeaf16
Victor Arteaga <xathovic@gmail.com>
2019-10-23 18:20:48 +0000

    Conditions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL || size < 1 || action == NULL)
+		return;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
