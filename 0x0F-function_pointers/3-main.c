b5a24d52a5466cb78a4b30cf9ba944616d62bed1
Victor Arteaga <xathovic@gmail.com>
2019-10-23 20:42:02 +0000

    Last fix I think

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,10 +9,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, calc;
+	int num1, num2, (*op)(int, int), calc;
 	char *operator;
 
-	if (argc > 4 || argc < 1)
+	if (argc > 4 || argc <= 1)
 	{
 		printf("Error\n");
 		exit(98);
@@ -21,13 +21,15 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	operator = argv[2];
 
-	calc = get_op_func(operator)(num1, num2);
+	op = get_op_func(operator);
 
-	if (calc == '\0')
+	if (op == '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
+	calc = op(num1, num2);
 	printf("%d\n", calc);
 	return (0);
 }
