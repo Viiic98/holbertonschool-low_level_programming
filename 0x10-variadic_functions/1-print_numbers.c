72844669bce4c7a95605a4301478580188efac97
Victor Arteaga <xathovic@gmail.com>
2019-10-25 20:02:14 +0000

    Printing numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_numbers - Printing numbers
+ *
+ * @separator: This will be between eacch argument
+ * @n: number of arguments
+ *
+ * Return: Nothing, is void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ar;
+
+	va_start(ar, n);
+
+	for (i = 0; i + 1 < n; i++)
+		printf("%d%s", va_arg(ar, int), separator);
+	printf("%d\n", va_arg(ar, int));
+	va_end(ar);
+}
