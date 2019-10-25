6761ee1e75624cf9ebd23705be4d9dfe97096851
Victor Arteaga <xathovic@gmail.com>
2019-10-25 22:54:51 +0000

    updating header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,9 @@ int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_c(void *);
+void print_i(void *);
+void print_f(void *);
+void print_s(void *);
 #endif /* VAR_FUNC */
