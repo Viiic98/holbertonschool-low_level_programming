eaedab4d7aa5fad37d1bb97dfc8f7aaf9d3e965c
viiic98 <xathovic@gmail.com>
2019-10-27 07:27:10 +0000

    Fix betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,37 +1,51 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
-  print_all - printing all
- *
- * @format: is a list of types of arguments
- * We can not change the value or pointer
- * Return: Nothing, is void
+ * print_c - Print chars
+ * @c: list of arguments
+ * Return: nothing
  */
-typedef struct ty
-{
-	char func;
-	void (*p)(va_list);
-} type;
 void print_c(va_list c)
 {
 	printf("%c", va_arg(c, int));
 }
+/**
+ * print_i - print integers
+ * @c: list of arguments
+ * Return: nothing
+ */
 void print_i(va_list c)
 {
 	printf("%d", va_arg(c, int));
 }
+/**
+ * print_f - print floats
+ * @c: list of arguments
+ * Return: nothing
+ */
 void print_f(va_list c)
 {
 	printf("%f", va_arg(c, double));
 }
+/**
+ * print_s - print strings
+ * @c: list of arguments
+ * Return: nothing
+ */
 void print_s(va_list c)
 {
 	char *s;
+
 	s = va_arg(c, char *);
 	if (*s == '\0')
 		s = "(nil)";
 	printf("%s", s);
 }
+/**
+ * print_all - Main function that print
+ * @format: list of format
+ * Return: nothing
+ */
 void print_all(const char * const format, ...)
 {
 	va_list ar;
@@ -42,7 +56,7 @@ void print_all(const char * const format, ...)
 		{'f', print_f},
 		{'s', print_s},
 	};
-	
+
 	i = 0;
 	va_start(ar, format);
 
