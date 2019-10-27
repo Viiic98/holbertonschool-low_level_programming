1e49282c3bfb4d1d91381335164db2b8b4495240
viiic98 <xathovic@gmail.com>
2019-10-27 07:27:43 +0000

    Add the structure to header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,13 +2,22 @@
 #define VAR_FUNC
 #include <stdarg.h>
 int _putchar(char);
-
+/**
+ * struct ty - store the formats
+ * @func: char of format
+ * @p: function for each format
+ */
+typedef struct ty
+{
+	char func;
+	void (*p)(va_list);
+} type;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_c(void *);
-void print_i(void *);
-void print_f(void *);
-void print_s(void *);
+void print_c(va_list);
+void print_i(va_list);
+void print_f(va_list);
+void print_s(va_list);
 #endif /* VAR_FUNC */
