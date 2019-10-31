a5a7064177cb6d6daf07cadf13535fa3ec1946e3
Victor Arteaga <xathovic@gmail.com>
2019-10-31 19:25:55 +0000

    Add changes to second file, now add nodes

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * add_node - add a new node
+ * @head: struct pointer
+ * @str: char that will be added
+ * Return: return a struct pointer
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr;
+	char *n_str;
+
+	if (str == NULL)
+		return (NULL);
+	n_str = strdup(str);
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	(*ptr).len = strlen(str);
+	(*ptr).str = n_str;
+	(*ptr).next = *head;
+	printf("[%d] %s\n", (*ptr).len, (*ptr).str);
+	return (ptr);
+}
