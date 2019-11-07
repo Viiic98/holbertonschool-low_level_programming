428e5395b01bbffb6f80ebb71c6abdc12943d113
Victor Arteaga <xathovic@gmail.com>
2019-11-07 22:11:58 +0000

    Add count and find the index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,14 +7,32 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i;
+	unsigned long int i = 1, count = 0, flag;
 	int byte;
 
 	if (index > 64)
 		return (-1);
 	if (n == 0 && index > 0)
 		return (-1);
-	i = n >> index;
+	i = i << 63;
+	while (i > 0)
+	{
+		if (n & i)
+			break;
+		i = i >> 1;
+	}
+	flag = i;
+	while (i >> 1 != 0)
+	{
+		count++, i = i >> 1;
+	}
+	i = flag;
+	while (i > 0)
+	{
+		if (count == index)
+			break;
+		i = i >> 1, count--;
+	}
 	if (n & i)
 		byte = 1;
 	else
