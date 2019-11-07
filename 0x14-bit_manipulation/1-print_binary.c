5f43b270a6d43e4b14589678122ed2b84064402f
Victor Arteaga <xathovic@gmail.com>
2019-11-07 20:53:55 +0000

    Add condition 0 at the beginning

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include "stdio.h"
 /**
  * print_binary - convert decimal to binary
  * @n: number is going to be converted
@@ -9,14 +8,17 @@ void print_binary(unsigned long int n)
 {
 	unsigned int i = 1 << 31;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	while (i > 0)
 	{
 		if (n & i)
 			break;
 		i = i >> 1;
 	}
-	if (i == 0)
-		_putchar('0');
 	while (i > 0)
 	{
 		(n & i) ? _putchar('1') : _putchar('0');
