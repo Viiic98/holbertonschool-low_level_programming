df8a808e8b07b002118293a352c2ed771bc46b16
Victor Arteaga <xathovic@gmail.com>
2019-11-07 18:59:41 +0000

    Add the first task

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Mandatory functions */
+unsigned int binary_to_uint(const char *b);
+
+/* Side functions */
+unsigned int pow2(unsigned int);
+#endif /* HOLBERTON_H */
