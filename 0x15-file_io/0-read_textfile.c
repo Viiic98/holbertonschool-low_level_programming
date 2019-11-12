728de4a3d78db5281cb62d2194e618397954b356
viiic98 <xathovic@gmail.com>
2019-11-12 21:53:09 +0000

    Add buffer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,18 +9,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t r, w;
-	char buf[letters];
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 
+	buf = malloc(letters);
+	if (buf == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 
 	r = read(fd, buf, letters);
 	if (r == -1)
 	{
+		free(buf);
 		close(fd);
 		return (0);
 	}
@@ -28,10 +36,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	w = write(STDOUT_FILENO, buf, r);
 	if (w == -1 || w != r)
 	{
+		free(buf);
 		close(fd);
 		return (0);
 	}
 
+	free(buf);
 	close(fd);
 
 	return (w);
