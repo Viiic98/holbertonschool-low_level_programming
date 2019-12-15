184061628f8a55919fb63c34aefd4ccd46dce6e9
viiic98 <xathovic@gmail.com>
2019-12-15 06:14:20 +0000

    Add free when is null

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	if (!*h && idx != 0)
+	{
+		free(new);
 		return (NULL);
+	}
 	if (idx == 0)
 	{
 		new->prev = NULL;
