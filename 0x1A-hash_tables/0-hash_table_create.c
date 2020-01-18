a909d71f6566e3b57ac1b8fafd5ba62a8313909d
viiic98 <xathovic@gmail.com>
2020-01-18 18:01:19 +0000

    Add task that create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,38 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - 
- * 
- *
+ * hash_table_create - Create a hash table
+ * @size: size of array
+ * Return: Pointer to new hash table
  **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new;
+	hash_node_t **n_array;
 
+	if (size <= 0)
+		return (NULL);
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->size = size;
+
+	n_array = malloc(sizeof(hash_node_t **));
+	if (n_array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	*n_array = calloc(size, sizeof(hash_node_t *));
+	if (*n_array == NULL)
+	{
+		free(new);
+		free(n_array);
+		return (NULL);
+	}
+	new->array = n_array;
+
+	return (new);
 }
