1acc1d72eb31b8412fa7119a79f58b90b329d6b4
viiic98 <xathovic@gmail.com>
2020-01-19 02:14:00 +0000

    Add some conditions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *tail, *new_n;
 	unsigned long int idx;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (key == NULL || value == NULL || ht == NULL)
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
