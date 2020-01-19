e6c7816a4c6b682d9d2ad29409c5c49e9f096de7
viiic98 <xathovic@gmail.com>
2020-01-19 02:03:06 +0000

    Fix the implementation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,31 +8,31 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *tail = (*ht->array), *new = NULL;
-	char *k, *v;
+	hash_node_t *tail, *new_n;
+	unsigned long int idx;
 
 	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
-	k = strdup(key);
-	v = strdup(value);
 
-	if (tail->key == NULL)
+	idx = key_index((const unsigned char *)key, ht->size);
+	tail = ht->array[idx];
+	while (tail)
 	{
-		tail->key = k;
-		tail->value = v;
-		tail->next = NULL;
-	}
-	else
-	{
-		new = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return (0);
-		while (tail->next != NULL)
+		if (strcmp(key, tail->key) == 0)
+		{
+			tail->value = strdup(value);
+			return (1);
+		}
+		else
 			tail = tail->next;
-		tail->next = new;
-		new->key = k;
-		new->value = v;
-		new->next = NULL;
 	}
+	new_n = malloc(sizeof(hash_node_t));
+	if (new_n == NULL)
+		return (0);
+	new_n->key = strdup(key);
+	new_n->value = strdup(value);
+	new_n->next = ht->array[idx];
+	ht->array[idx] = new_n;
+
 	return (1);
 }
