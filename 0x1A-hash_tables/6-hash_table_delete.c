fa0901443048fbcd5693b8347234e7031b586d4e
viiic98 <xathovic@gmail.com>
2020-01-19 21:26:07 +0000

    Add condition

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,7 +9,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *current, *tmp;
 	unsigned long int i;
 
-	if (ht != NULL)
+	if (ht != NULL && ht->array != NULL)
 	{
 		for (i = 0; i < ht->size; i++)
 		{
