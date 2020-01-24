2e2f12b7acfc2d3ef21b5cd914f2849e9d9ca4a8
Viiic98 <xathovic@gmail.com>
2020-01-24 16:08:03 +0000

    Add a free

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,6 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(key, tail->key) == 0)
 		{
+			free(tail->value);
 			tail->value = strdup(value);
 			return (1);
 		}
