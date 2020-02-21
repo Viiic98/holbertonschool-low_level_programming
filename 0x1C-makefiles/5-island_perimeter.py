e1de493b4f6ccc13e28dbcac8713e521287de82a
Viiic98 <xathovic@gmail.com>
2020-02-21 21:03:37 +0000

    Add task that find the parameter of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Island parameter """
+
+
+def island_perimeter(grid):
+    n = 0
+    for i in range(len(grid)):
+        f = 0
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                n += 1
+                if j + 1 < len(grid[i]):
+                    if grid[i][j + 1]:
+                        continue
+                elif i + 1 < len(grid):
+                    if grid[i + 1][j]:
+                        break
+    if n > 1:
+        return (n * 2) + 2
+    else:
+        return 4
