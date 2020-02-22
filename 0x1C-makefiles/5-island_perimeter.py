f7ee577ed4e091cd485193c534c300180f72b75a
viiic98 <xathovic@gmail.com>
2020-02-22 12:50:28 +0000

    Add condition when grid is out of range

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,15 +13,23 @@ def island_perimeter(grid):
                 if j + 1 < len(grid[i]):
                     if grid[i][j + 1] == 0:
                         l += 1
+                else:
+                    l += 1
                 if i - 1 >= 0:
                     if grid[i - 1][j] == 0:
                         l += 1
+                else:
+                    l += 1
                 if i + 1 < len(grid):
                     if grid[i + 1][j] == 0:
                         l += 1
+                else:
+                    l += 1
                 if j - 1 >= 0:
                     if grid[i][j - 1] == 0:
                         l += 1
+                else:
+                    l += 1
                 if l == 4:
                     return 4
                 else:
